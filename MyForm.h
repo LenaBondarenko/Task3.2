e5ae9f22d43fcaf63eaca7afc2e372334915d0a8
Лена Бондаренко <02ebondarenko@gmail.com>
2024-05-05 16:36:33 +0300

    Добавьте файлы проекта.

---
+++ MyForm.h
@@ -0,0 +1,220 @@
+#pragma once
+#include <math.h>
+#include <ctime>
+#include <random>
+
+namespace Task2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(72, 73);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(13, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"a";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(207, 73);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(13, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"b";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(335, 73);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(13, 13);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"k";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(42, 89);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(78, 20);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(171, 89);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(81, 20);
+			this->textBox2->TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(298, 89);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(81, 20);
+			this->textBox3->TabIndex = 5;
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(76, 127);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(242, 251);
+			this->listBox1->TabIndex = 6;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(42, 390);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(101, 50);
+			this->button1->TabIndex = 7;
+			this->button1->Text = L"ќбчислити1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(278, 390);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(101, 50);
+			this->button2->TabIndex = 8;
+			this->button2->Text = L"ќбчислити2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(12, 33);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(434, 17);
+			this->pictureBox1->TabIndex = 9;
+			this->pictureBox1->TabStop = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(458, 477);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		double a, b, k, h, y;
+		a = Convert::ToDouble(textBox1->Text);
+		b = Convert::ToDouble(textBox2->Text);
+		k = Convert::ToDouble(textBox3->Text);
+		h = 0.2;
+		listBox1->Items->Add("x\t|\ty");
+		listBox1->Items->Add("==================");
+		double x = a;
+		while (x <= b)
+		{
+			y = tan(1.0/x)+3.1;
+			listBox1->Items->Add(x.ToString("0") + "\t|\t" + y.ToString("0.00"));
+			x += h;
+		}
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	double a, b, k, h, z;
+	a = Convert::ToDouble(textBox1->Text);
+	b = Convert::ToDouble(textBox2->Text);
+	k = Convert::ToDouble(textBox3->Text);
+	h = 0.2;
+	listBox1->Items->Add("x\t|\tz");
+	listBox1->Items->Add("==================");
+	double x = a;
+	while (x <= b)
+	{
+		z = exp(x);
+		listBox1->Items->Add(x.ToString("0") + "\t|\t" + z.ToString("0.00"));
+		x += h;
+	}
+}
+};
+}
